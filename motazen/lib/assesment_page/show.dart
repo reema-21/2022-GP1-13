f4e8f26319bb87c7a3cfedf46fa8eb28944fe045
Rand-msb <105788148+Misfar-G2@users.noreply.github.com>
2022-11-02 11:00:19 +0300

    ff

--- motazen/lib/assesment_page/show.dart
+++ motazen/lib/assesment_page/show.dart
@@ -1,43 +1,43 @@
-// // ignore_for_file: camel_case_types
+// ignore_for_file: camel_case_types
 
-// import 'package:motazen/isar_service.dart';
+import 'package:motazen/isar_service.dart';
 
-// import 'package:flutter/material.dart';
-// import 'package:motazen/assesment_page/assesment_question_page_assignments.dart';
+import 'package:flutter/material.dart';
+import 'package:motazen/assesment_page/assesment_question_page_assignments.dart';
 
-// import 'QuastoinAssesment.dart';
+import 'QuastoinAssesment.dart';
 
-// class AssessmentQuestionsList extends StatefulWidget {
-//   final IsarService iser;
-//   final List<dynamic>? fixedAspect;
-//   final List<dynamic>? chosenAspect;
-//   const AssessmentQuestionsList(
-//       {super.key, required this.iser, this.fixedAspect, this.chosenAspect});
+class AssessmentQuestionsList extends StatefulWidget {
+  final IsarService iser;
+  final List<dynamic>? fixedAspect;
+  final List<dynamic>? chosenAspect;
+  const AssessmentQuestionsList(
+      {super.key, required this.iser, this.fixedAspect, this.chosenAspect});
 
-//   @override
-//   State<AssessmentQuestionsList> createState() => _showsState();
-// }
+  @override
+  State<AssessmentQuestionsList> createState() => _showsState();
+}
 
-// class _showsState extends State<AssessmentQuestionsList> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Center(
-//         child: FutureBuilder(
-//             future: AssessmentQuestions().createQuestionList(),
-//             builder: ((context, snapshot) {
-//               if (snapshot.connectionState == ConnectionState.done) {
-//                 List? question = snapshot.data;
-//                 return WheelOfLifeAssessmentPage(
-//                     isr: widget.iser,
-//                     question: question,
-//                     fixedAspect: widget.fixedAspect,
-//                     chosenAspect: widget.chosenAspect);
-//               } else {
-//                 return const CircularProgressIndicator();
-//               }
-//             })),
-//       ),
-//     );
-//   }
-// }
+class _showsState extends State<AssessmentQuestionsList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: FutureBuilder(
+            future: AssessmentQuestions().createQuestionList(),
+            builder: ((context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                List? question = snapshot.data;
+                return WheelOfLifeAssessmentPage(
+                    isr: widget.iser,
+                    question: question,
+                    fixedAspect: widget.fixedAspect,
+                    chosenAspect: widget.chosenAspect);
+              } else {
+                return const CircularProgressIndicator();
+              }
+            })),
+      ),
+    );
+  }
+}
