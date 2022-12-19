a7caa881a3a7aa975bda106860bb3f9b9d35a0ac
Rand-msb <105788148+Misfar-G2@users.noreply.github.com>
2022-12-19 22:24:34 +0300

    chnages

--- motazen/lib/pages/homepage/wheel_of_life/widget/pie_chart_sections.dart
+++ motazen/lib/pages/homepage/wheel_of_life/widget/pie_chart_sections.dart
@@ -1,8 +1,13 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:motazen/pages/homepage/wheel_of_life/progress_screen.dart';
+import '../../../../isar_service.dart';
 import '/entities/aspect.dart';
 
+
 class PieChartSections {
+
   List<PieChartSectionData> getSections(int touchedIndex, List<Aspect>? data) =>
       data!
           .asMap()
@@ -11,7 +16,7 @@ class PieChartSections {
 
             final isTouched = index == touchedIndex;
             if (isTouched) {
-              //add code to move to info page
+Get.to(() =>  ProgressScreen(aspect: data, isr: IsarService() ,)); // sending the aspect to the page
             }
 
             final value = PieChartSectionData(
